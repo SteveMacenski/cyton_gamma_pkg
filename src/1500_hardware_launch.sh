cca973bc3e26eb61d0a76c1dae3b03b4d42ed8bd
stevenmacenski@gmail.com <stevenmacenski@gmail.com>
2017-01-03 09:46:47 -0500

    single launcher shell script

--- src/1500_hardware_launch.sh
+++ src/1500_hardware_launch.sh
@@ -3,32 +3,36 @@
 source /opt/ros/indigo/setup.bash
 source /home/rdtintern/catkin_ws/devel/setup.bash
 
-echo "[1/5] start controller manager"
+echo "[1/6] start controller manager"
 
 xterm -e roslaunch cyton_1500_controllers controller_manager.launch &
 
 sleep 10
 
-echo "[2/5] start controllers"
+echo "[2/6] start controllers"
 
 xterm -e  roslaunch cyton_1500_controllers start_controller.launch &
 
 sleep 10
 
-echo "[3/5] start joint state publisher"
+echo "[3/6] start joint state publisher"
 
 xterm -e rosrun cyton_1500_controllers dynamixel_joint_state_publisher.py &
 
 sleep 5
 
-echo "[4/5] start planning execution window"
+echo "[4/6] start planning execution window"
 
 xterm -e roslaunch cyton_gamma_pkg gamma_1500_planning_execution.launch 2> /dev/null &
 
 sleep 20
 
-echo "[5/5] start command front end"
+echo "[5/6] start command front end"
 
 xterm -e python /home/rdtintern/catkin_ws/src/cyton_gamma_pkg/src/command_front_end.py 2> /dev/null &
+
+echo "[6/6] start feedback front end"
+
+xterm -e python /home/rdtintern/catkin_ws/src/cyton_gamma_pkg/src/feedback_front_end.py 2> /dev/null &
 
 echo "ready!"
