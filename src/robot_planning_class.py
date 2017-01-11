9fb7c5f5372bf6172ebe74ea7211ea49c6afa271
stevenmacenski@gmail.com <stevenmacenski@gmail.com>
2017-01-11 12:53:06 -0500

    making executable for rosrun

--- src/robot_planning_class.py
+++ src/robot_planning_class.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 from sensor_msgs.msg import JointState
 import copy
--- src/test_traj.py
+++ src/test_traj.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 import std_msgs
 from rospy import Duration
